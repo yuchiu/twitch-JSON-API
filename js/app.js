4df2275a777d264a6f2abbf3e7eb203a43bec604
yu <yuchiu2002@hotmail.com>
2017-11-05 13:30:18 -0500

    removed bootstrap, implemented add to list function for search result, set up div's containers for layout

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
-let channels = ['freecodecamp', 'membtv', 'escapeaoe', 'C9Sneaky', 'TheSpeedGamers', 'DkaneOfficial', 'T90Official', 'ZeroEmpires', 'DrDisRespectLIVE', 'KingGothalion'];
+let channels = ['freecodecamp', 'TheRace', 'TwitchPresents', 'C9Sneaky', 'TheSpeedGamers', 'DkaneOfficial', 'Ninja', 'Cryaotic', 'SoLLUMINATI', 'KingGothalion'];
 
-let ul = document.getElementById("channels");
-let listItems = ul.getElementsByTagName("li");
-let statusItems = ul.getElementsByClassName("status");
-let checkOutChannel = ul.getElementsByClassName('link');
-let channelName = ul.getElementsByClassName('title');
-let aButton = ul.getElementsByTagName('a');
-let picture = ul.getElementsByClassName('profile-pic');
-let searchitem = document.getElementById('searchBar');
-let searchChannel = document.getElementById('searchBarContent');
+const ul = document.getElementById("channels");
+const listItems = ul.getElementsByTagName("li");
+const statusItems = ul.getElementsByClassName("status");
+const checkOutChannel = ul.getElementsByClassName('link');
+const channelName = ul.getElementsByClassName('title');
+const aButton = ul.getElementsByTagName('a');
+const picture = ul.getElementsByClassName('profile-pic');
+const searchBar = document.getElementById('searchBar');
+const searchChannel = document.getElementById('searchBarContent');
+const addList = document.getElementById('add-list');
+
 
 (function fire() {
     $('#searchResult').hide();
@@ -24,13 +26,13 @@ function getChannelInfo(name, index) {
         if (data.stream != null) {
             picture[index].src = data.stream.channel.logo;
             statusItems[index].innerHTML = 'online';
-            statusItems[index].classList.add('class', 'badge-info');
+            statusItems[index].classList.add('class', 'online-status');
             listItems[index].classList.add('class', 'on');
             aButton[index].classList.add('class', 'btn-info');
             checkOutChannel[index].innerHTML = "Watch Live Stream!";
         } else {
             statusItems[index].innerHTML = 'offline';
-            statusItems[index].classList.add('class', 'badge-default');
+            statusItems[index].classList.add('class', 'offline-status');
             listItems[index].classList.add('class', 'off');
             aButton[index].classList.add('class', 'btn-default');
             checkOutChannel[index].innerHTML = "Browse The Channel";
@@ -45,7 +47,6 @@ function getOfflineProfilePicture(offlineChannel, num) {
         type: 'GET',
         error: function (XMLHttpRequest, textStatus, errorThrown) {
             channelName[num].innerHTML = 'ERROR, the channel "' + offlineChannel + '" does not exist';
-            console.log('status:' + XMLHttpRequest.status + ', status text: ' + XMLHttpRequest.statusText);
         },
         success: function (data) {
             picture[num].src = data.logo;
@@ -53,22 +54,30 @@ function getOfflineProfilePicture(offlineChannel, num) {
     });
 }
 
-searchitem.addEventListener('submit', (event) => {
+searchBar.addEventListener('submit', (event) => {
     event.preventDefault();
     let channelName = searchChannel.value;
-    console.log(channelName);
     getChannelInfo(channelName, 10);
     $('.on').hide();
     $('.off').hide();
     $('#searchResult').show();
 });
-
-$('.allOnline').click(function () {
+addList.addEventListener('click', (event)=>{
+    event.preventDefault();
+    let channelName = searchChannel.value;
+    channels.push(channelName)
+    console.log(channels)
+})
+$('#allChannels').click(function () {
+    $('.off').show();
+    $('.on').show();
+});
+$('#allOnline').click(function () {
     $('.off').hide();
     $('.on').show();
 });
 
-$('.allOffline').click(function () {
+$('#allOffline').click(function () {
     $('.on').hide();
     $('.off').show();
 });
