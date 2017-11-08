5a1863950272cf2b37b83ef9aeaeec63b55d1c38
yuchiu <yuchiu2002@hotmail.com>
2017-11-07 20:08:56 -0500

    implemented search dom structured for search result

--- js/app.js
+++ js/app.js
@@ -9,7 +9,6 @@ const aButton = channelList.getElementsByTagName('a');
 const picture = channelList.getElementsByClassName('profile-pic');
 const searchBar = document.getElementById('searchBar');
 const searchChannel = document.getElementById('searchBarContent');
-const addList = document.getElementById('add-list');
 
 
 (function fire() {
@@ -19,27 +18,58 @@ const addList = document.getElementById('add-list');
     }
 })()
 
+
 function getChannelInfo(name, index) {
     $.get("https://api.twitch.tv/kraken/streams/" + name + "?client_id=gvs1dph0z4nprptrzakral8izebdfr", function (data) {
+
+        $(".inner").append("<p>Test</p>");
         channelName[index].innerHTML = name;
-        checkOutChannel[index].href ="https://www.twitch.tv/"+name;
+        checkOutChannel[index].href = "https://www.twitch.tv/" + name;
         if (data.stream != null) {
             picture[index].src = data.stream.channel.logo;
             statusItems[index].innerHTML = 'online';
-            statusItems[index].classList.add('class', 'online-status');
-            listItems[index].classList.add('class', 'on');
+            statusItems[index].classList.add('online-status');
+            listItems[index].classList.add('on');
             checkOutChannel[index].innerHTML = "Watch Live Stream!";
         } else {
             statusItems[index].innerHTML = 'offline';
-            statusItems[index].classList.add('class', 'offline-status');
-            listItems[index].classList.add('class', 'off');
+            statusItems[index].classList.add('offline-status');
+            listItems[index].classList.add('off');
             checkOutChannel[index].innerHTML = "Browse The Channel";
-            getOfflineProfilePicture(name, index);
+            offlinePic(name, index);
         }
     });
 }
 
-function getOfflineProfilePicture(offlineChannel, num) {
+
+
+function getResultChannel(name) {
+    resultOfflinePic(name)
+    $.get("https://api.twitch.tv/kraken/streams/" + name + "?client_id=gvs1dph0z4nprptrzakral8izebdfr", function (data) {
+        $('#search-result').append(`<h2 id="result-title">${name}</h2><a id="result-link" href="https://www.twitch.tv/${name}" role="button" target="_blank">check out the channel</a>`);
+        if (data.stream != null) {
+            $('#search-result').append(`<div id="result-status" class="online-status">Online</div>`)
+        } else {
+            $('#search-result').append(`<div id="result-status" class="offline-status">Offline</div>`)
+        }
+    });
+}
+
+function resultOfflinePic(offlineChannel) {
+    $.ajax({
+        url: "https://api.twitch.tv/kraken/channels/" + offlineChannel + "?client_id=gvs1dph0z4nprptrzakral8izebdfr",
+        type: 'GET',
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+            $('#search-result').append(`<h2 id="result-title">Channel Dosen't Exist</h2>`);
+
+        },
+        success: function (data) {
+            $('#search-result').append(`<img src="${data.logo}" id="result-profile-pic">`)
+        }
+    });
+}
+
+function offlinePic(offlineChannel, num) {
     $.ajax({
         url: "https://api.twitch.tv/kraken/channels/" + offlineChannel + "?client_id=gvs1dph0z4nprptrzakral8izebdfr",
         type: 'GET',
@@ -51,39 +81,21 @@ function getOfflineProfilePicture(offlineChannel, num) {
         }
     });
 }
-function getResultChannel(name){
-    $.ajax({
-        url: "https://api.twitch.tv/kraken/channels/" + name + "?client_id=gvs1dph0z4nprptrzakral8izebdfr",
-        type: 'GET',
-        error: function (XMLHttpRequest, textStatus, errorThrown) {
-            console.log('faled')
-            $('#result-title').innerHTML = 'ERROR, the channel "' + name + '" does not exist';
-        },
-        success: function (data) {
-            console.log('sucess'+ data.logo)
-            $('#result-profile-pic').src = data.logo;
-        }
-    });
-}
-
 searchBar.addEventListener('submit', (event) => {
     event.preventDefault();
     let channelName = searchChannel.value;
-    getResultChannel(channelName)
     $('#result-placeholder').hide();
-    $('#searchResult').show();
+    $('#search-result').empty()
+    getResultChannel(channelName)
+    $('#search-result').show();
 });
-addList.addEventListener('click', (event)=>{
-    event.preventDefault();
-    let channelName = searchChannel.value;
-    channels.push(channelName)
-    console.log(channels)
-})
+
 $('#allChannels').click(function () {
     $('.off').show();
     $('.on').show();
 });
 $('#allOnline').click(function () {
+    console.log('dsadsasasd')
     $('.off').hide();
     $('.on').show();
 });
